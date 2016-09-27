52557973dfbeea8257cc84421b79ab766a3cc6a8
Andrew <apokorney@users.noreply.github.com>
2016-09-27 12:07:27 -0500

    Add files via upload

---
+++ fileReader.js
@@ -0,0 +1,24 @@
+/**
+ * Created by andrew.pokorney on 9/23/2016.
+ */
+
+
+var reader = require('fs');                                 //'fs' is the node file system wrapper
+reader.readFile                                             //create the readFile obj
+("Assignment1_names-1.txt", 'utf8', function(err,textData)  //so 'utf8' gives a blank unicode space
+{
+    textData = textData.slice(1, 100000);                   //syntax here is.... 'arr.slice([begin[, end]])'
+    textData.split('","')                                   //i'm  spliting by " , " here
+        .sort()                                             //sprts array e;elents
+        .forEach(function(simpsons){                        //forEach() method executes a provided function once per array element
+            console.log(simpsons) })                        //print out the sorted simpons arry
+}
+);
+
+
+
+//Some original stuff, keeping for reference
+// var fs = require("fs");
+// var text = fs.readFileSync("C:\\Users\\andrew.pokorney\\WebstormProjects\\Scratch\\Assignment1_names-1.txt" + '' );
+// var textByLine = text.split('","');
+// console.log(fs);
